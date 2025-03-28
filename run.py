e250b31c2841f9991adb00f15cf43fdf4974d245
Aditya <adimail2404@gmail.com>
2025-03-28 12:35:18 +0530

    logger

--- run.py
+++ run.py
@@ -1,11 +1,26 @@
 import signal
 import tornado.ioloop
 import asyncio
+import logging
 from server import make_app
+
+LOG_FILE = "notebookx.log"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[
+        logging.FileHandler(LOG_FILE, mode="w"),
+        logging.StreamHandler(),
+    ],
+)
+
+logger = logging.getLogger(__name__)
 
 
 def shutdown(loop):
-    print("\nShutting down Notebook X server...")
+    logger.info("Shutting down Notebook X server...")
     loop.stop()
 
 
@@ -16,15 +31,14 @@ INTRO = """
  | |\  | | |_| |   | |   | |___  | |_) | | |_| | | |_| | | . \     /  \\
  |_| \_|  \___/    |_|   |_____| |____/   \___/   \___/  |_|\_\   /_/\_\\
 
-
-
 """
 
 if __name__ == "__main__":
     app = make_app()
     app.listen(8888)
-    print(INTRO)
-    print("Notebook-X is running at http://localhost:8888")
+
+    logger.info("\n" + INTRO)
+    logger.info("Notebook-X is running at http://localhost:8888")
 
     loop = asyncio.get_event_loop()
 
