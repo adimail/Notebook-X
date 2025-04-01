ab7f38e2f8ad9867bb556a7be14a028db5a9622a
Aditya <adimail2404@gmail.com>
2025-04-01 19:42:43 +0530

    removed save, undo and rese from file editor

--- frontend/src/file.ts
+++ frontend/src/file.ts
@@ -1,14 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   const editor = document.getElementById("file-editor") as HTMLTextAreaElement;
-  const saveButton = document.getElementById(
-    "save-button",
-  ) as HTMLButtonElement;
-  const undoButton = document.getElementById(
-    "undo-button",
-  ) as HTMLButtonElement;
-  const resetButton = document.getElementById(
-    "reset-button",
-  ) as HTMLButtonElement;
   const saveIndicator = document.getElementById(
     "save-indicator",
   ) as HTMLElement;
@@ -23,32 +14,4 @@ document.addEventListener("DOMContentLoaded", () => {
   editor.addEventListener("input", () =>
     updateSaveIndicator(editor.value === originalContent),
   );
-
-  saveButton.addEventListener("click", async () => {
-    const response = await fetch("/api/save_file", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        path: window.location.pathname,
-        content: editor.value,
-      }),
-    });
-    const result = await response.json();
-    if (result.success) {
-      originalContent = editor.value;
-      updateSaveIndicator(true);
-    } else {
-      alert("Error saving file: " + result.message);
-    }
-  });
-
-  undoButton.addEventListener("click", () => {
-    editor.value = originalContent;
-    updateSaveIndicator(true);
-  });
-
-  resetButton.addEventListener("click", () => {
-    editor.value = "";
-    updateSaveIndicator(false);
-  });
 });
